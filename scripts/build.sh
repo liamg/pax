439a843fde718fe58f06e18cc29412d119d20bdf
Liam Galvin <liam@liam-galvin.co.uk>
2019-11-17 21:59:02 +0000

    Add travis build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
-env GO111MODULE=on go build ./cmd/pax
+BINARY=pax
+TAG=${TRAVIS_TAG:-development}
+GO111MODULE=on
+mkdir -p bin/darwin
+GOOS=darwin GOARCH=amd64 go build -o bin/darwin/${BINARY}-darwin-amd64 ./cmd/pax/
+mkdir -p bin/linux
+GOOS=linux GOARCH=amd64 go build -o bin/linux/${BINARY}-linux-amd64 ./cmd/pax/
+mkdir -p bin/windows
+GOOS=windows GOARCH=amd64 go build -o bin/windows/${BINARY}-windows-amd64.exe ./cmd/pax/
